5b41029b456352f186e23d0c621c27ad7b5fcdb6
arun.kumar <arun.kumar@SSTLAP357.srinsofttech.lan>
2024-09-30 16:29:15 +0530

    Commit_30092024

---
+++ src/main/java/org/Maven/Datatype.java
@@ -0,0 +1,15 @@
+package org.Maven;
+
+public class Datatype {
+	
+	public static void main(String[] args) {
+		
+		
+		int a = 1;
+		String epic = "Estes";
+		char proj = 'z';
+		double decimal = 5.55;
+		boolean action = true;
+	}
+
+}
